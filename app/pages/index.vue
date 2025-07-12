17340bd3e605bce52a886dd7861c55e2b044198e
andrei.kokoev@gmail.com <andrei.kokoev@gmail.com>
2025-07-12 14:10:21 -0500

    feat: add workspace configuration and update index page layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <main>
+    <div class="container">
+      <h1>Welcome to the New Index Page</h1>
+      <p>This is the updated version of the index page.</p>
+    </div>
+    <div class="content">
+      <p>Here you can find the latest updates and features.</p>
+    </div>
     <ImageGallery />
     <RedirectsPanel />
     <MessagesPanel />

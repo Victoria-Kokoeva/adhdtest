c8a3d853d2f829416cbffa05ca053840691bc304
andrei.kokoev@gmail.com <andrei.kokoev@gmail.com>
2025-07-12 16:18:57 -0500

    fix copy button

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -179,7 +179,13 @@ function submitAnswers() {
 }
 
 function copyShareUrl() {
-  navigator.clipboard.writeText(shareUrl.value);
+  const input = document.createElement('input');
+  input.value = shareUrl.value;
+  document.body.appendChild(input);
+  input.select();
+  input.setSelectionRange(0, 99999); // For mobile devices
+  document.execCommand('copy');
+  document.body.removeChild(input);
 }
 </script>
 
